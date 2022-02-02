357975a79301a8c889b6b1a5f314846e3048823b
Andrew Ellison <acannonellison@gmail.com>
2022-02-01 19:22:33 -0800

    Add images to public folder for portfolio

--- src/components/Body.js
+++ src/components/Body.js
@@ -11,27 +11,29 @@ import styles from "./Body.module.css";
 class Body extends Component {
   render() {
     return (
-      <div className={styles.mainContent}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="portfolio" element={<Portfolio />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="resume" element={<Resume />} />
-          {/* Unmatched route hit */}
-          <Route
-            path="*"
-            element={
-              <main style={{ padding: "1rem" }}>
-                <p>
-                  This is not the text you were expecting to see, nor I for that
-                  matter.
-                </p>
-              </main>
-            }
-          />
-        </Routes>
-      </div>
+      <main className={styles.mainContent}>
+        <section>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="portfolio" element={<Portfolio />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="resume" element={<Resume />} />
+            {/* Unmatched route hit */}
+            <Route
+              path="*"
+              element={
+                <main style={{ padding: "1rem" }}>
+                  <p>
+                    This is not the text you were expecting to see, nor I for
+                    that matter.
+                  </p>
+                </main>
+              }
+            />
+          </Routes>
+        </section>
+      </main>
     );
   }
 }
