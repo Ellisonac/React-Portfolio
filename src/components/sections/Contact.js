2dcf3a6e401351ffb0667ef3b439f5d8ce682772
Andrew Ellison <acannonellison@gmail.com>
2022-02-13 11:44:46 -0800

    Remove previous comments from Contact.js

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -2,19 +2,15 @@ import React, { useState } from 'react';
 import styles from "./Contact.module.css"
 
 function Contact() {
-  // Create state variables for the fields in the form
-  // We are also setting their initial values to an empty string
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [message, setMessage] = useState('');
 
   const handleInputChange = (e) => {
-    // Getting the value and name of the input which triggered the change
     const { target } = e;
     const inputType = target.name;
     const inputValue = target.value;
 
-    // Based on the input type, we set the state of either email, username, and password
     if (inputType === 'email') {
       setEmail(inputValue);
     } else if (inputType === 'name') {
@@ -25,13 +21,8 @@ function Contact() {
   };
 
   const handleFormSubmit = (e) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
     e.preventDefault();
 
-    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-    alert(`Hello ${name}`);
-
-    // If everything goes according to plan, we want to clear out the input after a successful registration.
     setName('');
     setMessage('');
     setEmail('');
